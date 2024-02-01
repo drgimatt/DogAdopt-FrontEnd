65f5c44152381ba36ace1cc73d71b33e7fca11d3
althealouise <87533603+althealouise@users.noreply.github.com>
2024-02-01 13:16:54 +0800

    added view dog activity for user
    disabled input fields

---
+++ app/src/main/java/com/doggo/dogadopt/activity/viewActivity.java
@@ -0,0 +1,16 @@
+package com.doggo.dogadopt.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.escandor.dogadopt.R;
+
+public class viewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
