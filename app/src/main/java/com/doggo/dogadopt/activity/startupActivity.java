b3594077393fd0a00ba706f8562ebc19d8c876b8
drgimatt <boxrox12@gmail.com>
2024-02-04 20:55:56 +0800

    Changed Keep me signed in option to remember me Implemented sharedpref for remember me

--- app/src/main/java/com/doggo/dogadopt/activity/startupActivity.java
+++ app/src/main/java/com/doggo/dogadopt/activity/startupActivity.java
@@ -2,9 +2,12 @@ package com.doggo.dogadopt.activity;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import com.doggo.dogadopt.retrofit.QueryProcessor;
 import com.escandor.dogadopt.R;
 
 import java.util.List;
+import java.util.prefs.Preferences;
 
 public class startupActivity extends AppCompatActivity {
 
@@ -23,22 +27,32 @@ public class startupActivity extends AppCompatActivity {
     EditText password;
     Button login;
     Button register;
+    CheckBox persistentCredentials;
     QueryProcessor queryProcessor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         setContentView(R.layout.activity_startup);
-        initializeComponents();
+        initializeComponents(sharedPref);
 
     }
 
 
-        private void initializeComponents() {
+        private void initializeComponents(SharedPreferences sharedPref) {
+
 
             username = findViewById(R.id.usernameField);
             password = findViewById(R.id.passwordField);
             login = findViewById(R.id.login_button);
             register = findViewById(R.id.signup_button);
+            persistentCredentials = findViewById(R.id.persistent_signin);
+            persistentCredentials.setChecked(sharedPref.getBoolean("persistentLogin",true));
+            username.setText(sharedPref.getString("username",""));
+            password.setText(sharedPref.getString("password",""));
+
+
 
             login.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -46,7 +60,7 @@ public class startupActivity extends AppCompatActivity {
                     if(username.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
                         Toast.makeText(startupActivity.this,"Please enter credentials", Toast.LENGTH_SHORT).show();
                     } else {
-                        checkAccount(username.getText().toString(),password.getText().toString());
+                        checkAccount(username.getText().toString(),password.getText().toString(),sharedPref);
                     }
                 }
             });
@@ -63,7 +77,7 @@ public class startupActivity extends AppCompatActivity {
 
         }
 
-        private void checkAccount(String uname, String pssword){
+        private void checkAccount(String uname, String pssword, SharedPreferences sharedPref){
 
             queryProcessor = new QueryProcessor();
             queryProcessor.AccountReadAll();
@@ -79,23 +93,35 @@ public class startupActivity extends AppCompatActivity {
                     for (Account acc: accList){
                         if((acc.getUsername().equals(uname)) && (acc.getPassword().equals(pssword))){
                             Account user = acc;
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            if(persistentCredentials.isChecked()){
+                            editor.putString("username",username.getText().toString());
+                            editor.putString("password",password.getText().toString());
+                            editor.putBoolean("persistentLogin",true);
+                            editor.commit();
+                            }
+                            else{
+                                editor.remove("username");
+                                editor.remove("password");
+                                editor.putBoolean("persistentLogin",false);
+                                editor.commit();
+                            }
                             if(user.getRole().equals("ADMIN")){
                                 Toast.makeText(startupActivity.this,"Login successful." + " Account Type: " + acc.getRole(), Toast.LENGTH_SHORT).show();
                                 Intent i = new Intent(getApplicationContext(), TestActivity.class);
                                 i.putExtra("userID",acc.getMyId());
                                 startActivity(i);
                                 finish();
-                                break;
+
                             } else if (user.getRole().equals("USER")){
                                 Toast.makeText(startupActivity.this,"Login successful." + " Account Type: " + acc.getRole(), Toast.LENGTH_SHORT).show();
                                 Intent i = new Intent(getApplicationContext(), addActivity.class);
                                 i.putExtra("userID",acc.getMyId());
                                 startActivity(i);
                                 finish();
-                                break;
+
                             } else {
                                 Toast.makeText(startupActivity.this,"Unsupported Account Type! Please use different account.", Toast.LENGTH_SHORT).show();
-                                break;
                             }
                         } else {
                             Toast.makeText(startupActivity.this,"Incorrect credentials. Please try again", Toast.LENGTH_SHORT).show();
